d11b6ba70732911b33fd81ddb953d1fcedff09e2
sos-robinson <robinsonsos.odhiambo@gmail.com>
2026-08-14 13:15:45 +0300

    Add static media routing url patterns

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+
+# THE FIX: This tells Django to serve files from STATIC_ROOT when accessed via STATIC_URL
+if settings.DEBUG or True:  # Forced to true so WhiteNoise catches routing on Vercel
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
