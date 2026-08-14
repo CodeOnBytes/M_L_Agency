2ae2361161585c010a4777ffc23e48bdb82aedd8
sos-robinson <robinsonsos.odhiambo@gmail.com>
2026-08-14 12:30:48 +0300

    Fix disallowed host error for Vercel

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wznv$p(xc0=cl01nk^$_$n9#)cy)w!8c(ib1%m3_g*$&fg+xgu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
